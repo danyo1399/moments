ef84929d1e136e581a6f7a2da9bd937f6d5fe5c5
dan <dan@nomail.com>
2025-06-02 00:19:42 +1200

    chore: make calculator private

--- calculator.go
+++ calculator.go
@@ -15,20 +15,21 @@ type subtracted struct {
 	value int
 }
 
-type CalculatorState struct {
+type calculatorState struct {
 	Value int
 }
 type calculator struct {
-	Aggregate[CalculatorState]
+	Aggregate[calculatorState]
 }
 
 const calculatorType AggregateType = "Calculator"
-var initStateFunc = func() CalculatorState {
-	return CalculatorState{0}
+
+var initStateFunc = func() calculatorState {
+	return calculatorState{0}
 }
 var newCalculatorAggregate = NewAggregateFactory(calculatorType, initStateFunc, reducer)
 
-func reducer(state CalculatorState, events ...any) CalculatorState {
+func reducer(state calculatorState, events ...any) calculatorState {
 	for _, event := range events {
 		switch e := event.(type) {
 		case added:
@@ -59,17 +60,16 @@ func (c *calculator) add(val int) {
 	c.Apply(evt, nil)
 }
 
-func NewCalculatorFromEvents(id string, events []any) *calculator {
-	agg := newCalculatorAggregate(WithEvents[CalculatorState](id, events))
+func newCalculatorFromEvents(id string, events []any) *calculator {
+	agg := newCalculatorAggregate(WithEvents[calculatorState](id, events))
 	calculator := calculator{
 		Aggregate: *agg,
 	}
 	return &calculator
 }
 
-func NewCalculator(id string) *calculator {
-
-	agg := newCalculatorAggregate(WithId[CalculatorState](id))
+func newCalculator(id string) *calculator {
+	agg := newCalculatorAggregate(WithId[calculatorState](id))
 	calculator := calculator{
 		Aggregate: *agg,
 	}
